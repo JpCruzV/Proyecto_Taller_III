b27297b4eb08732fac24d85ada9f1a2994dd764f
JpCruzV <88095820+JpCruzV@users.noreply.github.com>
2022-11-01 16:34:38 -0600

    First Attacks

---
+++ Assets/Scripts/Player Scripts/Fireball.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    Rigidbody rb;
+    float startCooldown = .5f;
+
+
+    private void Awake() {
+
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 3);
+        transform.parent = null;
+    }
+
+
+    private void FixedUpdate() {
+
+        startCooldown -= Time.fixedDeltaTime;
+
+        if (rb.velocity.y == 0 && rb.velocity.x == 0 && startCooldown <= 0) {
+
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.tag == "Player") {
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -5,6 +5,115 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 
+    private void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        startYScale = transform.localScale.y;
+        rb.freezeRotation = true;
+    }
+
+
+
+    private void Update() {
+
+        MyInput();
+        Flip();
+    }
+
+
+
+    private void FixedUpdate() {
+
+        MovePlayer();
+
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, floor);
+    }
+
+
+
+    void MyInput() {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+
+        //Double tap for running
+        if ((Input.GetKeyDown(KeyCode.D) && facingRight) || (Input.GetKeyDown(KeyCode.A) && !facingRight)) {
+
+            backwards = false;
+
+            if (ButtonCooler > 0 && ButtonCount == 1) {
+
+                running = true;
+            }
+            else {
+
+                ButtonCooler = 0.2f;
+                ButtonCount++;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) {
+
+            running = false;
+        }
+
+
+        //Moving Backwards
+        if (Input.GetKeyDown(KeyCode.A) && facingRight) {
+
+            backwards = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !facingRight) {
+
+            backwards = true;
+        }
+
+
+        //Jump
+        if (Input.GetKey(KeyCode.W) && readyToJump && grounded) {
+
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+
+        //Crouch
+        if (Input.GetKey(KeyCode.S) && grounded) {
+
+            crouching = true;
+            Crouch();
+        }
+        else if (Input.GetKeyUp(KeyCode.S)) {
+
+            crouching = false;
+            Crouch();
+        }
+
+
+        //Button cooldown
+        if (ButtonCooler > 0) {
+
+            ButtonCooler -= 1 * Time.deltaTime;
+        }
+        else {
+            ButtonCount = 0;
+        }
+
+
+        //Throw Fireball
+        if (Input.GetKey(KeyCode.U) && readyToThrow) {
+
+            FireballThrow();
+        }
+    }
+
+
+
+    #region Movement
+
+
     Rigidbody rb;
 
 
@@ -48,105 +157,6 @@ public class Player : MonoBehaviour {
     //Double Tap
     float ButtonCooler  = 0.5f;
     int ButtonCount = 0;
-
-
-
-    private void Start() {
-
-        rb = GetComponent<Rigidbody>();
-        startYScale = transform.localScale.y;
-        rb.freezeRotation = true;
-    }
-
-
-
-    private void Update() {
-
-        MyInput();
-        Flip();
-    }
-
-
-
-    private void FixedUpdate() {
-
-        MovePlayer();
-
-        // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, floor);
-    }
-
-
-
-    void MyInput() {
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-
-
-        //Double tap for running
-        if ( ( Input.GetKeyDown(KeyCode.D) && facingRight ) || ( Input.GetKeyDown(KeyCode.A) && !facingRight ) ) {
-
-            backwards = false;
-
-            if (ButtonCooler > 0 && ButtonCount == 1) {
-
-                running = true;
-            }
-            else {
-
-                ButtonCooler = 0.5f;
-                ButtonCount++;
-            }
-        } 
-        else if ( Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) ) {
-
-            running = false;
-        }
-
-
-        //Moving Backwards
-        if (Input.GetKeyDown(KeyCode.A) && facingRight) {
-
-            backwards = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.D) && !facingRight) {
-
-            backwards = true;
-        }
-
-
-        //Jump
-        if (Input.GetKey( KeyCode.W ) && readyToJump && grounded) {
-
-            readyToJump = false;
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-
-
-        //Crouch
-        if (Input.GetKey(KeyCode.S) && grounded) {
-
-            crouching = true;
-            Crouch();
-        }
-        else if (Input.GetKeyUp(KeyCode.S)) {
-
-            crouching = false;
-            Crouch();
-        }
-
-
-        //Button cooldown
-        if (ButtonCooler > 0) {
-
-            ButtonCooler -= 1 * Time.deltaTime;
-        }
-        else {
-            ButtonCount = 0;
-        }
-    }
 
 
 
@@ -217,4 +227,86 @@ public class Player : MonoBehaviour {
             transform.RotateAround(transform.position, transform.up, 180f);
         }
     }
+    #endregion
+
+
+
+    #region Attacks
+
+
+    [Header("Attacks references")]
+
+    [SerializeField] Transform fireballAttackPoint;
+    [SerializeField] GameObject fireballPrefab;
+    [SerializeField] Transform explosionCircleAttackPoint;
+    [SerializeField] GameObject explosionCirclePrefab;
+
+
+    [Header("Fireball variables")]
+
+    [SerializeField] float fireballCooldown;
+    [SerializeField] float fireballThrowForce;
+    [SerializeField] float fireballThrowUpwardsForce;
+    bool readyToThrow = true;
+
+
+    [Header("Circle Explosion variables")]
+
+    [SerializeField] float explosionCircleCooldown;
+    bool readyToUse = true;
+
+
+
+    void FireballThrow() {
+
+        readyToThrow = false;
+        GameObject fireball = Instantiate(fireballPrefab, fireballAttackPoint);
+        Rigidbody firaballRb = fireball.GetComponent<Rigidbody>();
+        Vector3 forceToAdd;
+
+
+        if (Input.GetKey(KeyCode.W)) {
+
+            forceToAdd = transform.up * fireballThrowUpwardsForce * 1.5f;
+            firaballRb.AddForce(forceToAdd, ForceMode.Impulse);
+        }
+        else if (crouching) {
+
+            fireball.transform.localScale = new Vector3(.5f, .5f, .5f);
+            forceToAdd = transform.right * fireballThrowForce * 1.5f;
+            firaballRb.AddForce(forceToAdd, ForceMode.Impulse);
+        }
+        else {
+
+            forceToAdd = transform.right * fireballThrowForce + transform.up * fireballThrowUpwardsForce;
+            firaballRb.AddForce(forceToAdd, ForceMode.Impulse);
+        }
+
+        Invoke(nameof(ResetThrow), fireballCooldown);
+    }
+
+
+
+    void createExplosionCircle() {
+
+        readyToUse = false;
+        GameObject explosionCircle = Instantiate(explosionCirclePrefab, explosionCircleAttackPoint);
+
+        Invoke(nameof(ResetAbilityUse), explosionCircleCooldown);
+    }
+
+
+
+    void ResetAbilityUse() {
+
+        readyToUse = true;
+    }
+
+
+
+    void ResetThrow() {
+
+        readyToThrow = true;
+    }
+    #endregion
 }
